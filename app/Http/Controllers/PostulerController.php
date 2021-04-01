c9a5a44600f3a52a8e0f484968d3b1be2c2564f5
Mehdi Mansour <mansourmehdi62@gmail.com>
2021-04-02 00:08:55 +0100

    Creation model, migration, controller des tables Entreprises et Postuler

---
+++ app/Http/Controllers/PostulerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Postuler;
+use Illuminate\Http\Request;
+
+class PostulerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Postuler  $postuler
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Postuler $postuler)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Postuler  $postuler
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Postuler $postuler)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Postuler  $postuler
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Postuler $postuler)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Postuler  $postuler
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Postuler $postuler)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_04_01_225110_create_postulers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostulersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postulers', function (Blueprint $table) {
+            $table->id();
+            $table->string("id_Student");
+            $table->string("id_Company");
+            $table->string("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postulers');
+    }
+}
